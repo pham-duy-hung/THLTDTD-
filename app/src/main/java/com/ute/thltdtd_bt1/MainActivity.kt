c1e44ef4678b4564065cf0174b2b003ff7233365
pham-duy-hung <hungphamduy456654@gmail.com>
2026-09-21 13:41:04 +0700

    add view

--- app/src/main/java/com/ute/thltdtd_bt1/MainActivity.kt
+++ app/src/main/java/com/ute/thltdtd_bt1/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val layoutMain = findViewById<LinearLayout>(R.id.layoutMain)
+        val myButton = Button(this).apply{
+            text="click here"
+            setOnClickListener { Toast.makeText(this@MainActivity,"Bạn đã ấn nút",Toast.LENGTH_SHORT).show() }
         }
+
+        layoutMain.addView(myButton)
     }
 }
